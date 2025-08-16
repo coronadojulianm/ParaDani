3befcdc7e434ffc47215eaadc71c94e19124f5b5
coronadojulianm <juliancoronadom@gmail.com>
2025-08-15 23:58:19 -0500

    2 commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-// Selección de elementos
+
 const images = document.querySelectorAll(".grid img");
 const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightbox-img");
 const lightboxDesc = document.getElementById("lightbox-desc");
 const closeBtn = document.querySelector(".close");
 
-// Abrir lightbox al hacer clic en una imagen
 images.forEach(img => {
   img.addEventListener("click", () => {
     lightbox.style.display = "flex";
@@ -13,13 +12,9 @@ images.forEach(img => {
     lightboxDesc.textContent = img.dataset.desc || "Sin descripción 💕";
   });
 });
-
-// Cerrar al hacer clic en la X
 closeBtn.addEventListener("click", () => {
   lightbox.style.display = "none";
 });
-
-// Cerrar al hacer clic fuera de la imagen
 lightbox.addEventListener("click", (e) => {
   if (e.target === lightbox) {
     lightbox.style.display = "none";
